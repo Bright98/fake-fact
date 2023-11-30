34ce8387f4891042d34ee0e0b97228698a8d965b
bright98 <www.hhsshhss@gmail.com>
2023-11-30 16:23:08 +0330

    change: connect to our backend

--- src/service/gpt.ts
+++ src/service/gpt.ts
@@ -1,26 +1,18 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export const AskFromGpt = async (content: string) => {
-    var url = "https://api.openai.com/v1/chat/completions";
-    var body = {
-        model: "gpt-3.5-turbo",
-        messages: [
-            {
-                role: "user",
-                content: content,
-            },
-        ],
-    };
+    var url = `${process.env.REACT_APP_BASE_URL}/api/write-article`;
+    console.log(url);
+    var body = { content: content };
     var response;
 
     try {
         response = await axios.post(url, body, {
             headers: {
-                Authorization: `Bearer ${process.env.REACT_APP_GPT_API_KEY}`,
+                "Content-Type": "application/json",
             },
         });
-        if (response.data !== null)
-            return response.data["choices"][0]["message"]["content"];
+        if (response.data !== null) return response.data["data"];
         else return null;
     } catch (e) {
         console.log(e);
